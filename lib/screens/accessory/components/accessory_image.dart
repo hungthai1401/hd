41ad54f99be0b23e3fa055f1c8e330370777e414
Nguyen Hung Thai <hungthai1401.it@gmail.com>
2019-12-22 17:56:53 +0700

    [IMPLEMENT] zoom image feature

--- lib/screens/accessory/components/accessory_image.dart
+++ lib/screens/accessory/components/accessory_image.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hd/models/accessory_image/accessory_image_model.dart';
+import 'package:hd/screens/accessory/components/gallery_photo_view_wrapper.dart';
 
 class AccessoryImage extends StatelessWidget {
   final AccessoryImageModel accessoryImage;
@@ -7,21 +8,39 @@ class AccessoryImage extends StatelessWidget {
   AccessoryImage({@required this.accessoryImage})
       : assert(accessoryImage is AccessoryImageModel);
 
+  void _openGallery(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GalleryPhotoViewWrapper(
+          item: NetworkImage(accessoryImage.image),
+          backgroundDecoration: const BoxDecoration(
+            color: Colors.black,
+          ),
+          initialIndex: 1,
+          scrollDirection: Axis.vertical,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        constraints: new BoxConstraints.expand(
-          height: 400.0,
-        ),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(accessoryImage.image),
-            fit: BoxFit.fill,
+      child: GestureDetector(
+        onTap: () => _openGallery(context),
+        child: Container(
+          constraints: new BoxConstraints.expand(
+            height: 400.0,
+          ),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(accessoryImage.image),
+              fit: BoxFit.fill,
+            ),
           ),
         ),
       ),
     );
-    ;
   }
 }
--- lib/screens/accessory/components/captured_image.dart
+++ lib/screens/accessory/components/captured_image.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:hd/screens/accessory/components/gallery_photo_view_wrapper.dart';
 
 class CapturedImage extends StatelessWidget {
   final File capturedImage;
@@ -9,38 +10,57 @@ class CapturedImage extends StatelessWidget {
   CapturedImage(
       {@required this.capturedImage, @required this.onDeleteCapturedImage});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      constraints: new BoxConstraints.expand(
-        height: 400.0,
-      ),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: Image.file(capturedImage).image,
-          fit: BoxFit.fill,
+  void _openGallery(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GalleryPhotoViewWrapper(
+          item: FileImage(capturedImage),
+          backgroundDecoration: const BoxDecoration(
+            color: Colors.black,
+          ),
+          initialIndex: 1,
+          scrollDirection: Axis.vertical,
         ),
       ),
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-            right: 0.0,
-            child: GestureDetector(
-              onTap: () => onDeleteCapturedImage(),
-              child: Align(
-                alignment: Alignment.topRight,
-                child: CircleAvatar(
-                  radius: 14.0,
-                  backgroundColor: Colors.transparent,
-                  child: Icon(
-                    Icons.close,
-                    color: Colors.red,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _openGallery(context),
+      child: Container(
+        constraints: new BoxConstraints.expand(
+          height: 400.0,
+        ),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: Image.file(capturedImage).image,
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              right: 0.0,
+              child: GestureDetector(
+                onTap: () => onDeleteCapturedImage(),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: CircleAvatar(
+                    radius: 14.0,
+                    backgroundColor: Colors.transparent,
+                    child: Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
